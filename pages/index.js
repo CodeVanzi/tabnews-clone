18c61e64e2a1d1f5ab9abec45117aa881a572ba2
Graciel Covanzi <41657320+CodeVanzi@users.noreply.github.com>
2024-03-23 14:02:29 +0000

    correção página index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-  return <h1>Página em construção</h1><p></p><h2>Em breve teremos novidades</h2>;
-
+  return (
+    <div>
+      <h1>Página em construção</h1>
+      <p></p>
+      <h2>Em breve teremos novidades</h2>
+    </div>
+  );
 }
 
 export default Home;
